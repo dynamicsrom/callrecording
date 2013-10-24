91a6469330766a72dca7304cb9885224f2a869f6
ultrashot <ultrashotru@gmail.com>
2013-10-25 00:21:52 +0400

    !Second try to fix formatting

--- record.asm
+++ record.asm
@@ -21,11 +21,11 @@ result_dataready = 6
 
 code32
 org codezone
-    BL newsection
-    CMP R0, result_dataready
-    BEQ dataready_inner
-    CMP R0, result_waitagain
-    B waitagain
+    BL          newsection
+    CMP         R0, result_dataready
+    BEQ         dataready_inner
+    CMP         R0, result_waitagain
+    B           waitagain
 ;-------------------------------
 
 
@@ -50,24 +50,24 @@ org newsection
     MOV         R0, 0
 
 Mixing:                                     ;if current recorder is second, we mix channels
-    MOV		    R0, R11
-    MOV		    R1, R9
-    BL		    PerformMixing
-    LDR 	    R11, dword_B00	
-    BL		    PerformCopy
-    MOV 	    R0, result_dataready
+    MOV         R0, R11
+    MOV         R1, R9
+    BL          PerformMixing
+    LDR         R11, dword_B00    
+    BL          PerformCopy
+    MOV         R0, result_dataready
 exit:
     LDMFD       SP!, {R1-R11,LR}
     BX          LR
 ;-------------------------------
 PerformCopy:                                ;and then copy everything to output
-    STMFD	    SP!, {R1-R6,LR}
-    LDR 	    R5, dword_fromStartVocoderInput
-    LDR		    R6, dword_100A6B4C
+    STMFD       SP!, {R1-R6,LR}
+    LDR         R5, dword_fromStartVocoderInput
+    LDR         R6, dword_100A6B4C
     LDR         R2, [R5]
     LDR         R3, dword_100A6AAC
     ;copying
-    MOV		    R1, R0                      ; void *
+    MOV         R1, R0                      ; void *
     LDR         R0, [R3, R2, lsl 0x2]       ; void *
     MOV         R2, 0x140                   ; size_t
     BL          memcpy
@@ -79,21 +79,21 @@ PerformCopy:                                ;and then copy everything to output
     ADD         R3, R3, 1
     STR         R3, [R5]
     CMP         R3, 9
-    BLS		    PerformCopy_exit		
+    BLS         PerformCopy_exit        
     MOV         R3, 0
     STR         R3, [R5]
 PerformCopy_exit:
-    LDMFD	    SP!, {R1-R6,LR}
-    BX		    LR
+    LDMFD       SP!, {R1-R6,LR}
+    BX          LR
 ;-------------------------------
 PerformMixing:
-    STMFD	    SP!, {R0-R5,LR}
-    MOV		    R4, R0
-    MOV		    R5, R1
+    STMFD       SP!, {R0-R5,LR}
+    MOV         R4, R0
+    MOV         R5, R1
 
 __MixAgain:
-    MOV 	    R1, R4
-    MOV		    R3, R5
+    MOV         R1, R4
+    MOV         R3, R5
     LDRSH       R2, [R1,R0]!
     LDRSH       R3, [R3,R0]
     ADD         R0, R0, 2
@@ -102,8 +102,8 @@ __MixAgain:
     STRH        R3, [R1]
     BLT         __MixAgain
 
-    LDMFD	    SP!, {R0-R5,LR}
-    BX		    LR
+    LDMFD       SP!, {R0-R5,LR}
+    BX          LR
 ;-------------------------------
 align 4
 dword_600 dw 0xBC0EF600
