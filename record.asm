600f787f40d3120fe70d8da22680c7fbb07a473b
ultrashot <ultrashotru@gmail.com>
2013-10-25 00:19:26 +0400

    !Fixed formatting

--- record.asm
+++ record.asm
@@ -31,79 +31,79 @@ org codezone
 
 code32
 org newsection
-    STMFD   SP!, {R1-R11,LR}
-    LDR R4, [SP,0x0C] 
-    LDR R5, [SP,0x10]
-    LDR R6, [SP,0x14]
-    LDR R8, [SP,0x1C]
-    LDR R9, [SP,0x20]
-    LDR R10,[SP,0x24]
+    STMFD       SP!, {R1-R11,LR}
+    LDR         R4, [SP,0x0C] 
+    LDR         R5, [SP,0x10]
+    LDR         R6, [SP,0x14]
+    LDR         R8, [SP,0x1C]
+    LDR         R9, [SP,0x20]
+    LDR         R10,[SP,0x24]
 
-    LDR R1, [SP,0x40]               ;0x30+0x10
-    CMP R1, 2                       ;if current recorder isn't second one, 
-    MOVNE R0, result_waitagain      ;it shouldn't be processed
-    BNE exit                        ;and we wait for next handles.
+    LDR         R1, [SP,0x40]               
+    CMP         R1, 2                       ;if current recorder isn't second one, 
+    MOVNE       R0, result_waitagain        ;it shouldn't be processed
+    BNE         exit                        ;and we wait for next handles.
 
     ;TODO: loading fake channels.
-    LDR R11, dword_B00              ;!!!not safe!!!
-    LDR R9, dword_600               ;!!!not safe!!!
-    MOV R0, 0
+    LDR         R11, dword_B00              ;!!!not safe!!!
+    LDR         R9, dword_600               ;!!!not safe!!!
+    MOV         R0, 0
 
-Mixing:                             ;if current recorder is second, we mix channels
-    MOV		R0, R11
-    MOV		R1, R9
-    BL		PerformMixing
-    LDR 	R11, dword_B00	
-    BL		PerformCopy
-    MOV 	R0, result_dataready
+Mixing:                                     ;if current recorder is second, we mix channels
+    MOV		    R0, R11
+    MOV		    R1, R9
+    BL		    PerformMixing
+    LDR 	    R11, dword_B00	
+    BL		    PerformCopy
+    MOV 	    R0, result_dataready
 exit:
-    LDMFD   SP!, {R1-R11,LR}
-    BX      LR
+    LDMFD       SP!, {R1-R11,LR}
+    BX          LR
 ;-------------------------------
-PerformCopy:                        ;and then copy everything to output
-    STMFD	SP!, {R1-R6,LR}
-    LDR 	R5, dword_fromStartVocoderInput
-    LDR		R6, dword_100A6B4C
-    LDR     R2, [R5]
-    LDR     R3, dword_100A6AAC
+PerformCopy:                                ;and then copy everything to output
+    STMFD	    SP!, {R1-R6,LR}
+    LDR 	    R5, dword_fromStartVocoderInput
+    LDR		    R6, dword_100A6B4C
+    LDR         R2, [R5]
+    LDR         R3, dword_100A6AAC
     ;copying
-    MOV		R1, R0                  ; void *
-    LDR     R0, [R3, R2, lsl 0x2]   ; void *
-    MOV     R2, 0x140               ; size_t
-    BL      memcpy
-    LDR     R3, [R5]
+    MOV		    R1, R0                      ; void *
+    LDR         R0, [R3, R2, lsl 0x2]       ; void *
+    MOV         R2, 0x140                   ; size_t
+    BL          memcpy
+    LDR         R3, [R5]
     ;Setting size
-    MOV     R2, 0x140 
-    STR     R2, [R6, R3, lsl 0x2]
+    MOV         R2, 0x140 
+    STR         R2, [R6, R3, lsl 0x2]
     ;
-    ADD     R3, R3, 1
-    STR     R3, [R5]
-    CMP     R3, 9
-    BLS		PerformCopy_exit		
-    MOV     R3, 0
-    STR     R3, [R5]
+    ADD         R3, R3, 1
+    STR         R3, [R5]
+    CMP         R3, 9
+    BLS		    PerformCopy_exit		
+    MOV         R3, 0
+    STR         R3, [R5]
 PerformCopy_exit:
-    LDMFD	SP!, {R1-R6,LR}
-    BX		LR
+    LDMFD	    SP!, {R1-R6,LR}
+    BX		    LR
 ;-------------------------------
 PerformMixing:
-    STMFD	SP!, {R0-R5,LR}
-    MOV		R4, R0
-    MOV		R5, R1
+    STMFD	    SP!, {R0-R5,LR}
+    MOV		    R4, R0
+    MOV		    R5, R1
 
 __MixAgain:
-    MOV 	R1, R4
-    MOV		R3, R5
-    LDRSH   R2, [R1,R0]!
-    LDRSH   R3, [R3,R0]
-    ADD     R0, R0, 2
-    CMP     R0, 0x140
-    ADD     R3, R3, R2
-    STRH    R3, [R1]
-    BLT     __MixAgain
+    MOV 	    R1, R4
+    MOV		    R3, R5
+    LDRSH       R2, [R1,R0]!
+    LDRSH       R3, [R3,R0]
+    ADD         R0, R0, 2
+    CMP         R0, 0x140
+    ADD         R3, R3, R2
+    STRH        R3, [R1]
+    BLT         __MixAgain
 
-    LDMFD	SP!, {R0-R5,LR}
-    BX		LR
+    LDMFD	    SP!, {R0-R5,LR}
+    BX		    LR
 ;-------------------------------
 align 4
 dword_600 dw 0xBC0EF600
